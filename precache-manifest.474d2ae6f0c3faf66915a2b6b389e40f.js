06c15fa0626fb8efa5c256775cf6a6870a9d0b42
Obi <obiadrian@gmail.com>
2020-10-23 14:35:07 +0300

    Updates

---
+++ precache-manifest.474d2ae6f0c3faf66915a2b6b389e40f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a211cd308171074a15f5fcae8d46647b",
+    "url": "/Yelpy/index.html"
+  },
+  {
+    "revision": "310cf4335ce363db80d9",
+    "url": "/Yelpy/static/css/main.5ee54515.chunk.css"
+  },
+  {
+    "revision": "36d1765395aee7d80e22",
+    "url": "/Yelpy/static/js/2.e6d9204c.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/Yelpy/static/js/2.e6d9204c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "310cf4335ce363db80d9",
+    "url": "/Yelpy/static/js/main.e9debde2.chunk.js"
+  },
+  {
+    "revision": "8b9ba9c1682e6109d173",
+    "url": "/Yelpy/static/js/runtime-main.601046d3.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Yelpy/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Yelpy/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
